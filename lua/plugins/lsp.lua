a47e91b84b371bc1c3109a17061dc97f06ef2b47
Kevin Johnson <kljohnson@verisk.com>
2023-11-01 09:17:44 -0600

    Add git integration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,118 +1,140 @@
 return {
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
     dependencies = {
+      { "tpope/vim-sleuth" },
+      { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+      {"folke/neodev.nvim"},
+
       --- Uncomment these if you want to manage LSP servers from neovim
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
 
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },
+      { "neovim/nvim-lspconfig" },
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lua' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'L3MON4D3/LuaSnip' },
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-nvim-lua" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "L3MON4D3/LuaSnip" },
     },
     priority = 500,
     config = function()
-      local lsp = require('lsp-zero')
+      local lsp = require("lsp-zero")
 
-      lsp.preset('recommended')
+      lsp.preset("recommended")
 
-      local cmp = require('cmp')
+      local cmp = require("cmp")
       local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
       cmp.setup({
         sources = {
-          { name = 'nvim_lua' },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
+          { name = "nvim_lua" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
         },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-          ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-          ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        })
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+          ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+          ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
       })
-
 
       lsp.on_attach(function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
 
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>cd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "gd", function()
+          vim.lsp.buf.definition()
+        end, opts)
+        vim.keymap.set("n", "K", function()
+          vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set("n", "<leader>ws", function()
+          vim.lsp.buf.workspace_symbol()
+        end, opts)
+        vim.keymap.set("n", "<leader>cd", function()
+          vim.diagnostic.open_float()
+        end, opts)
+        vim.keymap.set("n", "[d", function()
+          vim.diagnostic.goto_next()
+        end, opts)
+        vim.keymap.set("n", "]d", function()
+          vim.diagnostic.goto_prev()
+        end, opts)
+        vim.keymap.set("n", "<leader>ca", function()
+          vim.lsp.buf.code_action()
+        end, opts)
+        vim.keymap.set("n", "<leader>gr", function()
+          vim.lsp.buf.references()
+        end, opts)
+        vim.keymap.set("n", "<leader>rn", function()
+          vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set("n", "<C-h>", function()
+          vim.lsp.buf.signature_help()
+        end, opts)
         vim.keymap.set("n", "<leader>f", function()
           vim.lsp.buf.format()
         end)
       end)
 
-      require('mason').setup({
+      require("mason").setup({
         ensure_installed = {
-          'js-debug-adapter',
-          'netcoredbg',
-        }
+          "js-debug-adapter",
+          "netcoredbg",
+        },
       })
-      require('mason-lspconfig').setup({
+      require("mason-lspconfig").setup({
         -- Replace the language servers listed here
         -- with the ones you want to install
         ensure_installed = {
-          'azure_pipelines_ls',
-          'bashls',
-          'clangd',
-          'cssls',
-          'dockerls',
-          'eslint',
-          'html',
-          'jsonls',
-          'lua_ls',
-          'marksman',
-          'omnisharp',
-          'powershell_es',
-          'tsserver',
-          'vale_ls',
-          'yamlls',
+          "azure_pipelines_ls",
+          "bashls",
+          "clangd",
+          "cssls",
+          "dockerls",
+          "eslint",
+          "html",
+          "jsonls",
+          "lua_ls",
+          "marksman",
+          "omnisharp",
+          "powershell_es",
+          "tsserver",
+          "vale_ls",
+          "yamlls",
         },
         handlers = {
           lsp.default_setup,
         },
       })
 
-      lsp.setup();
+      lsp.setup()
 
-      require('lspconfig').tsserver.setup({
+      require("lspconfig").tsserver.setup({
         on_attach = function(client, bufnr)
           local opts = { buffer = bufnr, remap = false }
 
-          vim.keymap.set("n", "<leader>co",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.organizeImports.ts" },
-                  diagnostics = {},
-                },
-              })
-            end, opts)
+          vim.keymap.set("n", "<leader>co", function()
+            vim.lsp.buf.code_action({
+              apply = true,
+              context = {
+                only = { "source.organizeImports.ts" },
+                diagnostics = {},
+              },
+            })
+          end, opts)
         end,
         keys = {
           {
